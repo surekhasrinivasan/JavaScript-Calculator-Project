1e5a5f77cbdee216175a7dbf4a3ba653afaa513f
surekha <surekhasrao@gmail.com>
2017-12-27 04:02:42 +0000

    Javascript calculator final commit

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
-function getValue0() {
-	document.calci.output.value += "0";
-}
-
-function getValue1() {
-	document.calci.output.value += "1";
-}
-
-function getValue2() {
-	document.calci.output.value += "2";
-}
-
-function getValue3() {
-	document.calci.output.value += "3";
-}
-
-function getValue4() {
-	document.calci.output.value += "4";
-}
-
-function getValue5() {
-	document.calci.output.value += "5";
-}
-
-function getValue6() {
-	document.calci.output.value += "6";
-}
-
-function getValue7() {
-	document.calci.output.value += "7";
-}
-
-function getValue8() {
-	document.calci.output.value += "8";
-}
-
-function getValue9() {
-	document.calci.output.value += "9";
-}
-
 function doAdd() {
-	document.calci.output.value += "+";
+	dispOperator("+");
 }
 
 function doSub() {
-	document.calci.output.value += "-";
+	dispOperator("-");
 }
 
 function doMulti() {
-	document.calci.output.value += "*";
+	dispOperator("*");
 }
 
 function doDiv() {
-	document.calci.output.value += "/";
+	dispOperator("/");
 }
 
-function getDot() {
-	document.calci.output.value += ".";
+function getValue(value) {
+	if (document.calci.equalsClicked.value == "true") {
+		document.calci.output.value = value;
+		document.calci.equalsClicked.value = "false";
+	} else {
+		document.calci.output.value += value;
+	}
 }
 
 function evalu() {
+	document.calci.equalsClicked.value = "true";
 	var result = eval(document.calci.output.value);
 	document.calci.output.value = result;
+	document.calci.storedNum.value = '';
 }
 
 function doClear() {
 	document.calci.output.value = '';
+	document.calci.storedNum.value = '';
+}
+
+function dispOperator(operator) {
+	var output = document.calci.output.value;
+	var lastChar = output.substring(output.length - 1);
+	var operatorPresent = false;
+	document.calci.equalsClicked.value = "false";
+	if (lastChar.indexOf('+') != -1) {
+		operatorPresent = true;
+	} else if (lastChar.indexOf('-') != -1) {
+		operatorPresent = true;
+	} else if (lastChar.indexOf('*') != -1) {
+		operatorPresent = true;
+	} else if (lastChar.indexOf('/') != -1) {
+		operatorPresent = true;
+	}
+	if (operatorPresent == false) {
+		document.calci.output.value += operator;
+	}
+	document.calci.storedNum.value = output + operator;
+}
+
+function clEntry() {
+	var output = document.calci.output.value;
+	var lastChar = output.substring(document.calci.storedNum.value.length - 1);
+	if (lastChar.indexOf('+') != -1) {
+		output = output.substring(0, document.calci.storedNum.value.length);
+	} else if (lastChar.indexOf('-') != -1) {
+		output = output.substring(0, document.calci.storedNum.value.length);
+	} else if (lastChar.indexOf('*') != -1) {
+		output = output.substring(0, document.calci.storedNum.value.length);
+	} else if (lastChar.indexOf('/') != -1) {
+		output = output.substring(0, document.calci.storedNum.value.length);
+	} else {
+		output = document.calci.storedNum.value;
+	}
+	document.calci.output.value = output;
 }
